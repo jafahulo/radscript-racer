ff8ce4b44de5ab276b3ff59edbfc394ab94afb06
wScottSh <william.scott.sheppard@gmail.com>
2017-12-09 16:17:46 -0600

    adds basic car moving funstionality

--- racelogic.js
+++ racelogic.js
@@ -1,3 +1,13 @@
 // define the number of key presses it takes to win the game.
 // move the car at increments of 100% / the number of keypresses.
 // when it goes to >= 100%, then display that there's a winner
+
+
+// window.getComputedStyle(one).left  <=== returns the value of the left position.
+
+console.log("this is tstill working?");
+
+const moveCar = (num) =>
+{
+  document.getElementById("one").style.left = num + "%";
+}
