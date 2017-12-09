b90e7f765bc99e64a6b1da416df191b9b467cd0c
wScottSh <william.scott.sheppard@gmail.com>
2017-12-09 16:03:17 -0600

    fixes up the styling

--- racelogic.js
+++ racelogic.js
@@ -1,5 +1,3 @@
 // define the number of key presses it takes to win the game.
 // move the car at increments of 100% / the number of keypresses.
 // when it goes to >= 100%, then display that there's a winner
-
-console.log("it's working");
