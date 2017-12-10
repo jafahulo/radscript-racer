d6eabd80b4adffb531387ede5df1473bf99ac1aa
wScottSh <william.scott.sheppard@gmail.com>
2017-12-09 18:47:03 -0600

    move car function works

--- racelogic.js
+++ racelogic.js
@@ -1,16 +1,16 @@
-// define the number of key presses it takes to win the game.
 // move the car at increments of 100% / the number of keypresses.
 // when it goes to >= 100%, then display that there's a winner
 
-
-// window.getComputedStyle(one).left  <=== returns the value of the left position.
-
-let carPosition = document.getElementById("one").style.left;
 let carMax = 100;
+let pressDistance = 0;
+const carArray = ["one", "two"];
+let staticPercent = 0;
 
 // Updates the carOnePosition variable. Enter the % of the race completed.
-const updateCarPosition = (percent) => { // add the second param to define car
-  carPosition = percent + "%";
+const changeCarPosition = (num, car) => {
+  // debugger;
+  staticPercent = num;
+  document.getElementById(carArray[car]).style.left = num + "%";
 }
 
 // Checks to see if a car has crossed the finish line
@@ -22,13 +22,29 @@ const checkForWinner = () => {
 }
 
 // this divides the number of button presses needed to win the game
-const setPresses = (presses) => {
+const setPressNumber = (presses) => {
+  let distance = carMax / presses;
+  return pressDistance = distance;
+}
 
+// function that actually moves the car "press"
+const moveCarOne = () => {
+  // add event listener
+  return changeCarPosition((staticPercent + pressDistance), 0);
+}
+
+const moveCarTwo = () => {
+  // add event listener
+  return changeCarPosition((staticPercent + pressDistance), 1);
 }
 
 // function with initial settings for game
 const initializeGame = () => {
-updateCarOnePosition(0);
+  // debugger;
+changeCarPosition(0, 0);
+changeCarPosition(0, 1);
+setPressNumber(50);
+
 }
 
 // inititialize the game
